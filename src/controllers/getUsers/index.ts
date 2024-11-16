bedf01cbc4927f0a2e260adcd59d13beb87b3be7
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-11-17 02:28:20 +0300

    feat:add command GET all users

--- src/controllers/getUsers/index.ts
+++ src/controllers/getUsers/index.ts
@@ -1,13 +1,12 @@
 import fs from 'fs';
 import http from 'http';
-import { User } from '../../tsInterfaces/index';
 export default async function getUsers(
   userDataPath: string,
   res: http.ServerResponse<http.IncomingMessage>,
 ): Promise<void> {
-  const data = await fs.promises.readFile(userDataPath, { encoding: 'utf-8' });
-  const users: User[] = JSON.parse(data);
+  const users: string = await fs.promises.readFile(userDataPath, {
+    encoding: 'utf-8',
+  });
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  console.log(users);
   res.end(users);
 }
