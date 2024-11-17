8f8a133cfc551ca2c66a098aedd1d3e4df35d498
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-11-17 14:43:24 +0300

    feat:add get command

--- src/controllers/getUsers/index.ts
+++ src/controllers/getUsers/index.ts
@@ -1,12 +1,32 @@
 import fs from 'fs';
 import http from 'http';
+import { User } from 'src/tsInterfaces';
+import { validate } from 'uuid';
 export default async function getUsers(
   userDataPath: string,
   res: http.ServerResponse<http.IncomingMessage>,
+  searchId: string | undefined,
 ): Promise<void> {
   const users: string = await fs.promises.readFile(userDataPath, {
     encoding: 'utf-8',
   });
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(users);
+  if (searchId !== '' && searchId !== undefined) {
+    if (!validate(searchId)) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Search id is not valid' }));
+      return;
+    }
+    const usersArr: User[] = JSON.parse(users);
+    const indexOfSearch = usersArr.findIndex((e: User) => e.id === searchId);
+    if (indexOfSearch != -1) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(usersArr[indexOfSearch]));
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Search id does not exist' }));
+    }
+  } else {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(users);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,17 @@ import path from 'path';
 import { getUsers, postUsers } from './controllers/index';
 const server = http.createServer(async (req, res) => {
   const userDataPath = path.join(__dirname, 'usersData', 'usersData.json');
-  if (req.url === '/api/users') {
-    if (req.method === 'POST') {
+  const id = req.url?.split('/')[3];
+  try {
+    if (req.url === '/api/users' && req.method === 'POST') {
       postUsers(userDataPath, res, req);
-    } else if (req.method === 'GET') {
-      getUsers(userDataPath, res);
     }
-  } else {
+    if (req.url?.match('/api/users')) {
+      if (req.method === 'GET') {
+        getUsers(userDataPath, res, id);
+      }
+    }
+  } catch {
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Wrong address' }));
   }
